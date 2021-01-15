f63c39a025f3e20e9b503924e9f84bd4162f4a16
Antoine209 <73525571+Antoine209@users.noreply.github.com>
2021-01-15 12:02:28 +0100

    ajout ORM

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -53,8 +53,9 @@ class AuthController extends AbstractController
         if ($finalForm->isSubmitted() && $finalForm->isValid()) {
             $data = $finalForm->getData();
 
+            var_dump($finalForm);
             var_dump($data);
-            var_dump($data.$this->get(0));
+            var_dump($data[1]);
 
             /*
             $entityManager->setEmail($data);
