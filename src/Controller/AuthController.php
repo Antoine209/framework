5102d8bc39eae7c44bc72958de7e9508759e7978
Antoine209 <73525571+Antoine209@users.noreply.github.com>
2021-01-15 14:22:14 +0100

    ajout ORM

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -56,7 +56,7 @@ class AuthController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            return $this->redirectToRoute('/users');
+            return $this->redirectToRoute('user.html.twig');
         }
         return $this->render("login.html.twig", ["formulaire" => $finalForm->createView()]);
     }
